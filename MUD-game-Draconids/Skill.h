d0ab1476d2e6bbbd6dd8a77376622d1c14a5aab9
dx2331lxz <3434055686@qq.com>
2023-08-23 23:21:26 +0800

    写了Role类的基本元素，创建了需要的类

---
+++ MUD-game-Draconids/Skill.h
@@ -0,0 +1,12 @@
+#pragma once
+#include"main.h"
+
+class Skill
+{
+public:
+	Skill();
+	~Skill();
+
+private:
+
+};
--- MUD-game-Draconids/main.h
+++ MUD-game-Draconids/main.h
@@ -1,3 +1,13 @@
 #pragma once
 #include<iostream>
+#include<string>
+#include"Role.h"
+#include"Task.h"
+#include"Good.h"
+#include"Skill.h"
+#include"Bag.h"
+#include <vector>
+#include <memory>
+#include <map>
+#include <iomanip> // 包含这个头文件以使用格式化输出相关功能
 using namespace std;
