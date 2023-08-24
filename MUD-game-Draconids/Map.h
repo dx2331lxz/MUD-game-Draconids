369a20ae1ab6d2e2a3b8abc27ee68bb77fe7d18c
seaseal <942671896@qq.com>
2023-08-24 11:49:25 +0800

    Revert "Revert "暂存""
    This reverts commit add886edb581b78e0c3a929093e08fcbb8ff6985.

--- MUD-game-Draconids/Map.h
+++ MUD-game-Draconids/Map.h
@@ -26,10 +26,38 @@ private:
 	vector<string> posname = { "卡塞尔学院","青铜城","北京","日本","俄罗斯","北极","仕兰中学" };
 };
 
-class MapNode
+class School_Map {   //卡塞尔学院地图
+public:
+	School_Map();
+	void showmap();
+	void move(string &action);
+private:
+	vector<vector<string>>posname = { {"教室","训练室"},{"地窖","装备部"},{"休息室","图书馆"} };
+	int pos[2];
+};
+
+class MapNode   //青铜城专用地图块
 {
 public:
-	 
+	 //不同的人物指针
+	MapNode() {}
+	//MapNode(MapNode *root);
+	vector<MapNode*>& GetChildren()
+	{
+		return children;
+	}
+	void AddChildren(MapNode* NewChildren);
 private:
+	vector<MapNode*> children;//存储相邻的地图节点
+};
 
+
+class TreeMap {
+public:
+	TreeMap()
+		: root(nullptr)
+	{}
+	void AddNote(MapNode* newone,MapNode *parent);
+private:
+	MapNode* root;//根节点存储入口位置
 };
